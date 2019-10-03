b2adf1b4b7a84683e1ac58c3e9b56d6f1601531b
Davor Budimir <47507294+BudimirDavor@users.noreply.github.com>
2019-10-03 18:09:07 +0200

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Davor
+ * Date: 3.10.2019.
+ * Time: 16:00
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace DemoApi
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button OKbtn;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.OKbtn = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.SuspendLayout();
+			// 
+			// OKbtn
+			// 
+			this.OKbtn.Location = new System.Drawing.Point(371, 343);
+			this.OKbtn.Name = "OKbtn";
+			this.OKbtn.Size = new System.Drawing.Size(75, 23);
+			this.OKbtn.TabIndex = 0;
+			this.OKbtn.Text = "OK";
+			this.OKbtn.UseVisualStyleBackColor = true;
+			this.OKbtn.Click += new System.EventHandler(this.OKbtnClick);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(45, 36);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(401, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(45, 102);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(401, 20);
+			this.textBox2.TabIndex = 3;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(45, 299);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(104, 24);
+			this.radioButton1.TabIndex = 4;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "radioButton1";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(182, 299);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(104, 24);
+			this.radioButton2.TabIndex = 5;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "radioButton2";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.Location = new System.Drawing.Point(317, 299);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(104, 24);
+			this.radioButton3.TabIndex = 6;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "radioButton3";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(486, 396);
+			this.Controls.Add(this.radioButton3);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.OKbtn);
+			this.Name = "MainForm";
+			this.Text = "DemoApi";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Davor
+ * Date: 3.10.2019.
+ * Time: 16:00
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DemoApi
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void OKbtnClick(object sender, EventArgs e)
+		{
+			if (radioButton1.Checked==true)
+			{
+				textBox2.Text=textBox1.Text;
+				textBox1.Text="";
+			}
+		}
+	}
+}
